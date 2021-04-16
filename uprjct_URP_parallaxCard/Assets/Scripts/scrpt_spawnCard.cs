e8426b48b38055532c3726f37a8d051e4b0b0015
3p0d_on_off <43069214+3P0D@users.noreply.github.com>
2021-04-16 17:15:17 +0200

    SandrineB Imported

--- uprjct_URP_parallaxCard/Assets/Scripts/scrpt_spawnCard.cs
+++ uprjct_URP_parallaxCard/Assets/Scripts/scrpt_spawnCard.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -6,65 +6,67 @@ using TMPro;
 public class scrpt_spawnCard : MonoBehaviour
 {
 
-    public GameObject[] m_cards;
-    [Space(5)]
-    public TMP_Text m_nameText;
-    public TMP_Text m_artNameText;
+	public GameObject[] m_cards;
+	[Space(5)]
+	public TMP_Text m_nameText;
+	public TMP_Text m_artNameText;
 
-    //Private
-    int currentIndex = 0;
-    GameObject currentObject;
-    scrpt_cardProfile _cardProfile;
-    GameObject _spawner;
+	//Private
+	int currentIndex = 0;
+	GameObject currentObject;
+	scrpt_cardProfile _cardProfile;
+	GameObject _spawner;
 
 
-    void Start()
-    {
-        _spawner = this.gameObject;
-        currentObject = Instantiate(m_cards[currentIndex]);
-        currentObject.transform.SetParent(_spawner.transform);
+	void Start()
+	{
+		_spawner = this.gameObject;
+		currentObject = Instantiate(m_cards[currentIndex]);
+		currentObject.transform.SetParent(_spawner.transform);
 
-        _cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
-        m_artNameText.text = _cardProfile.m_artistname;
-        Camera.main.backgroundColor = _cardProfile.m_background;
-    }
+		_cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
+		m_nameText.text = _cardProfile.m_name;
+		m_artNameText.text = _cardProfile.m_artistname;
+		Camera.main.backgroundColor = _cardProfile.m_background;
+	}
 
-    public void Update()
-    {
-        currentObject.transform.rotation = _spawner.transform.rotation;
-    }
+	public void Update()
+	{
+		currentObject.transform.rotation = _spawner.transform.rotation;
+	}
 
-    public void Previous()
-    {
-        Destroy(currentObject);
-        currentIndex--;
-        if (currentIndex > m_cards.Length || currentIndex < 0)
-        {
-            currentIndex = m_cards.Length - 1;
-        }
+	public void Previous()
+	{
+		Destroy(currentObject);
+		currentIndex--;
+		if (currentIndex > m_cards.Length || currentIndex < 0)
+		{
+			currentIndex = m_cards.Length - 1;
+		}
 
-        currentObject = Instantiate(m_cards[currentIndex]);
-        currentObject.transform.SetParent(_spawner.transform);
+		currentObject = Instantiate(m_cards[currentIndex]);
+		currentObject.transform.SetParent(_spawner.transform);
 
-        _cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
-        m_artNameText.text = _cardProfile.m_artistname;
-        Camera.main.backgroundColor = _cardProfile.m_background;
-    }
+		_cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
+		m_nameText.text = _cardProfile.m_name;
+		m_artNameText.text = _cardProfile.m_artistname;
+		Camera.main.backgroundColor = _cardProfile.m_background;
+	}
 
-    public void Next()
-    {
-        Destroy(currentObject);
-        currentIndex++;
-        if (currentIndex > (m_cards.Length - 1) || currentIndex < 0)
-        {
-            currentIndex = 0;
-        }
-        currentObject = Instantiate(m_cards[currentIndex]);
-        currentObject.transform.SetParent(_spawner.transform);
+	public void Next()
+	{
+		Destroy(currentObject);
+		currentIndex++;
+		if (currentIndex > (m_cards.Length - 1) || currentIndex < 0)
+		{
+			currentIndex = 0;
+		}
+		currentObject = Instantiate(m_cards[currentIndex]);
+		currentObject.transform.SetParent(_spawner.transform);
 
-        _cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
-        m_nameText.text = _cardProfile.m_name;
-        m_artNameText.text = _cardProfile.m_artistname;
-        Camera.main.backgroundColor = _cardProfile.m_background;
-    }
+		_cardProfile = currentObject.GetComponent<scrpt_cardProfile>();
+		m_nameText.text = _cardProfile.m_name;
+		m_artNameText.text = _cardProfile.m_artistname;
+		Camera.main.backgroundColor = _cardProfile.m_background;
+	}
 }
